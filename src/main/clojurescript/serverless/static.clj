a98f8dd344d868c3fd88b72c1b5168a6050c8f73
Matthew Molloy <whamtet@gmail.com>
2021-02-17 06:38:45 +0700

    spit serverless.yml

---
+++ src/main/clojurescript/serverless/static.clj
@@ -0,0 +1,32 @@
+(ns serverless.static
+  (:require
+    [clojure.string :as string]))
+
+(defn get-lines []
+  (->> (-> "serverless.format.yml" slurp (.split "\n"))
+       (drop-while #(-> % .trim (.startsWith "hello:") not))
+       (take-while #(-> % .trim not-empty))
+       (split-at 3)
+       (map #(string/join "\n" %))))
+
+(defn section [[preface method-section] function-name]
+  (let [preface (.replace preface "hello" function-name)
+        method-section (.replace method-section "hello" function-name)]
+    (string/join
+      "\n"
+      (list*
+        preface
+        (for [method ["get" "post" "put" "patch" "delete"]]
+          (.replace method-section "get" method))))))
+
+(defn spit-config [methods]
+  (let [lines (get-lines)]
+    (spit "serverless.yml"
+          (.replace
+            (slurp "serverless.format.yml")
+            (string/join "\n" lines)
+            (->> methods
+                 (map #(section lines %))
+                 (string/join "\n"))))))
+
+
